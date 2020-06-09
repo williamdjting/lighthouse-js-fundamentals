49248ea3708f684d0ea5eb1d8dc15d04d7501d69
William Ting <williamdjting@gmail.com>
2020-06-09 02:47:03 +0000

    adjusted packing-list.js

--- odd.js
+++ odd.js
@@ -1,10 +1,32 @@
-function isOdd(num) {
-  if (num % 2 === 0) {
-    return true;
-  } else { 
-    return false;
+const stations = [
+  ['Big Bear Donair', 10, 'restaurant'],
+  ['Bright Lights Elementary', 50, 'school'],
+  ['Moose Mountain Community Centre', 45, 'community centre']
+];
+
+function chooseStations (stations) {
+  const goodStations = []
+
+  for (const station of stations) {
+    const capacity = station[1]
+    
+    if (capacity >= 20) {
+      const type = station[2]
+
+      if (type === "school" || type === "community centre") {
+        goodStations.push(station[0])
+      }
+    }
   }
-  }
-  
-  console.log("3 is odd: " + isOdd(3));
-  console.log("8 is odd: " + isOdd(8));
+  return goodStations
+
+}
+
+
+
+
+
+console.log(chooseStations(stations))
+
+
+
